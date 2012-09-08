4a320fa5ece047a7d5006c105538b1f39b4d6885
oliver.lamy@gmail.com <oliver.lamy@gmail.com@e07f450a-d586-5016-98a4-c0cbeb9252a2>
2012-09-08 19:38:55 +0000

    [maven-release-plugin]  copy for tag maven-scm-provider-svnjava-2.0.5

---
+++ tags/maven-scm-provider-svnjava-2.0.5/src/test/java/org/apache/maven/scm/provider/svn/svnjava/command/remoteinfo/SvnJavaRemoteInfoCommandTest.java
@@ -0,0 +1,66 @@
+package org.apache.maven.scm.provider.svn.svnjava.command.remoteinfo;
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ *   http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+
+import org.apache.maven.scm.command.remoteinfo.RemoteInfoScmResult;
+import org.apache.maven.scm.provider.svn.svnjava.repository.SvnJavaScmProviderRepository;
+import org.codehaus.plexus.PlexusTestCase;
+import org.tmatesoft.svn.core.SVNURL;
+
+/**
+ * @author Olivier Lamy
+ */
+public class SvnJavaRemoteInfoCommandTest
+    extends PlexusTestCase
+{
+    public void testRemoteInfo()
+        throws Exception
+    {
+        SvnJavaRemoteInfoCommand c = new SvnJavaRemoteInfoCommand();
+        SvnJavaScmProviderRepository svnScmProviderRepository = new SvnJavaScmProviderRepository(
+            SVNURL.parseURIEncoded( "http://svn.apache.org/repos/asf/maven/scm/trunk" ),
+            "http://svn.apache.org/repos/asf/maven/scm/trunk" );
+        RemoteInfoScmResult r = c.executeRemoteInfoCommand( svnScmProviderRepository, null, null );
+        System.out.println( "r:" + r.toString() );
+        assertTrue( r.getTags().containsKey( "maven-scm-1.7" ) );
+
+    }
+
+    public void testExist()
+        throws Exception
+    {
+        SvnJavaRemoteInfoCommand svnRemoteInfoCommand = new SvnJavaRemoteInfoCommand();
+
+        SvnJavaScmProviderRepository svnScmProviderRepository = new SvnJavaScmProviderRepository(
+            SVNURL.parseURIEncoded( "http://svn.apache.org/repos/asf/maven/scm/trunk/" ),
+            "http://svn.apache.org/repos/asf/maven/scm/trunk/" );
+        assertTrue( svnRemoteInfoCommand.remoteUrlExist( svnScmProviderRepository, null ) );
+    }
+
+    public void testNotExist()
+        throws Exception
+    {
+        SvnJavaRemoteInfoCommand svnRemoteInfoCommand = new SvnJavaRemoteInfoCommand();
+
+        SvnJavaScmProviderRepository svnScmProviderRepository = new SvnJavaScmProviderRepository(
+            SVNURL.parseURIEncoded( "http://svn.apache.org/repos/asf/maven/scm/trunk/foo/bar/beer" ),
+            "http://svn.apache.org/repos/asf/maven/scm/trunk/foo/bar/beer" );
+        assertFalse( svnRemoteInfoCommand.remoteUrlExist( svnScmProviderRepository, null ) );
+    }
+}
