9621bf37036dc8e111f2b86afbd74974b87ac355
celinio <dan-fernandes.consultant@dgfip.finances.gouv.fr>
2023-02-28 14:15:00 +0100

    Fixed tests with correct path

--- src/test/java/org/apache/maven/scm/provider/svn/svnjava/command/checkin/SvnJavaCheckInCommandTckTest.java
+++ src/test/java/org/apache/maven/scm/provider/svn/svnjava/command/checkin/SvnJavaCheckInCommandTckTest.java
@@ -1,5 +1,10 @@
 package org.apache.maven.scm.provider.svn.svnjava.command.checkin;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -20,31 +25,294 @@ package org.apache.maven.scm.provider.svn.svnjava.command.checkin;
  */
 
 import java.io.File;
+import java.io.FileWriter;
+import java.io.PrintWriter;
+import java.util.List;
+import java.util.Map;
 
+import org.apache.maven.scm.ScmFile;
+import org.apache.maven.scm.ScmFileSet;
+import org.apache.maven.scm.ScmFileStatus;
+import org.apache.maven.scm.command.add.AddScmResult;
+import org.apache.maven.scm.command.checkin.CheckInScmResult;
+import org.apache.maven.scm.command.checkout.CheckOutScmResult;
+import org.apache.maven.scm.provider.ScmProvider;
 import org.apache.maven.scm.provider.svn.command.checkin.SvnCheckInCommandTckTest;
 import org.apache.maven.scm.provider.svn.svnjava.SvnJavaScmTestUtils;
+import org.apache.maven.scm.util.FilenameUtils;
+import org.codehaus.plexus.util.FileUtils;
+import org.codehaus.plexus.util.IOUtil;
+import org.codehaus.plexus.util.StringUtils;
+import org.junit.Assume;
+import org.junit.Test;
 
 /**
  * @author <a href="mailto:dh-maven@famhq.com">David Hawkins</a>
- * @version $Id: SvnJavaCheckInCommandTckTest.java 96 2009-03-25 23:17:29Z oliver.lamy $
+ * @version $Id: SvnJavaCheckInCommandTckTest.java 96 2009-03-25 23:17:29Z
+ *          oliver.lamy $
  */
-public class SvnJavaCheckInCommandTckTest
-    extends SvnCheckInCommandTckTest
-{
+public class SvnJavaCheckInCommandTckTest extends SvnCheckInCommandTckTest {
 
-    /** {@inheritDoc} */
-    public void initRepo()
-        throws Exception
-    {
-        SvnJavaScmTestUtils.initializeRepository( getRepositoryRoot() );
-    }
+	/** {@inheritDoc} */
+	@Override
+	public void initRepo() throws Exception {
+		SvnJavaScmTestUtils.initializeRepository(getRepositoryRoot());
+	}
 
-    /** {@inheritDoc} */
-    public String getScmUrl()
-        throws Exception
-    {
-        return SvnJavaScmTestUtils.getScmUrl( new File( getRepositoryRoot(), "trunk" ) );
-    }
+	/** {@inheritDoc} */
+	@Override
+	public String getScmUrl() throws Exception {
+		return SvnJavaScmTestUtils.getScmUrl(new File(getRepositoryRoot(), "trunk"));
+	}
 
-    
+	@Test
+	public void testCheckInCommandFilesetWithBasedirOtherThanWorkingCopyRoot() throws Exception {
+		ScmProvider scmProvider = getScmManager().getProviderByUrl(getScmUrl());
+
+		Assume.assumeFalse("Local provider does not properly support basedir",
+				scmProvider.getScmType().equals("local"));
+		// Make sure that the correct files was checked out
+		File fooJava = new File(getWorkingCopy(), "src/main/java/Foo.java");
+
+		assertFalse("check Foo.java doesn't yet exist", fooJava.canRead());
+
+		// Change the files
+		createFooJava(fooJava);
+
+		AddScmResult addResult = scmProvider.add(getScmRepository(),
+				new ScmFileSet(new File(getWorkingCopy(), "src"), "main/java/Foo.java", null));
+
+		assertResultIsSuccess(addResult);
+
+		List<ScmFile> files = addResult.getAddedFiles();
+		assertNotNull(files);
+		assertEquals(1, files.size());
+
+		String fileWithoutPathUnixStyle = createFilePathWithoutCompletePath(getWorkingCopy().toString(),
+				files.get(0).getPath());
+		// SCM-998: filename separators not yet harmonized
+		assertEquals("/src/main/java/Foo.java", FilenameUtils.normalizeFilename(fileWithoutPathUnixStyle));
+
+		CheckInScmResult result = getScmManager().checkIn(getScmRepository(),
+				new ScmFileSet(new File(getWorkingCopy(), "src"), "**/Foo.java", null), "Commit message");
+
+		assertResultIsSuccess(result);
+
+		files = result.getCheckedInFiles();
+		assertNotNull(files);
+		assertEquals(1, files.size());
+
+		ScmFile file1 = files.get(0);
+		assertEquals(ScmFileStatus.CHECKED_IN, file1.getStatus());
+
+		String file1WithoutPathUnixStyle = createFilePathWithoutCompletePath(getWorkingCopy().toString(),
+				file1.getPath());
+		assertPath("/src/main/java/Foo.java", file1WithoutPathUnixStyle);
+
+		CheckOutScmResult checkoutResult = getScmManager().checkOut(getScmRepository(),
+				new ScmFileSet(getAssertionCopy()));
+
+		assertResultIsSuccess(checkoutResult);
+
+		fooJava = new File(getAssertionCopy(), "src/main/java/Foo.java");
+
+		assertTrue("check can read Foo.java", fooJava.canRead());
+
+	}
+
+	@Test
+	public void testCheckInCommandPartialFileset() throws Exception {
+		// Make sure that the correct files was checked out
+		File fooJava = new File(getWorkingCopy(), "src/main/java/Foo.java");
+
+		File barJava = new File(getWorkingCopy(), "src/main/java/Bar.java");
+
+		File readmeTxt = new File(getWorkingCopy(), "readme.txt");
+
+		assertFalse("check Foo.java doesn't yet exist", fooJava.canRead());
+
+		assertFalse("check Bar.java doesn't yet exist", barJava.canRead());
+
+		assertTrue("check can read readme.txt", readmeTxt.canRead());
+
+		// Change the files
+		createFooJava(fooJava);
+
+		createBarJava(barJava);
+
+		changeReadmeTxt(readmeTxt);
+
+		AddScmResult addResult = getScmManager().getProviderByUrl(getScmUrl()).add(getScmRepository(),
+				new ScmFileSet(getWorkingCopy(), "src/main/java/Foo.java", null));
+
+		assertResultIsSuccess(addResult);
+
+		CheckInScmResult result = getScmManager().checkIn(getScmRepository(),
+				new ScmFileSet(getWorkingCopy(), "**/Foo.java", null), "Commit message");
+
+		assertResultIsSuccess(result);
+
+		List<ScmFile> files = result.getCheckedInFiles();
+		assertNotNull(files);
+		assertEquals(1, files.size());
+
+		ScmFile file1 = files.get(0);
+		assertEquals(ScmFileStatus.CHECKED_IN, file1.getStatus());
+
+		String file1WithoutPathUnixStyle = createFilePathWithoutCompletePath(getWorkingCopy().toString(),
+				file1.getPath());
+		assertPath("/src/main/java/Foo.java", file1WithoutPathUnixStyle);
+
+		CheckOutScmResult checkoutResult = getScmManager().checkOut(getScmRepository(),
+				new ScmFileSet(getAssertionCopy()));
+
+		assertResultIsSuccess(checkoutResult);
+
+		fooJava = new File(getAssertionCopy(), "src/main/java/Foo.java");
+
+		barJava = new File(getAssertionCopy(), "src/main/java/Bar.java");
+
+		readmeTxt = new File(getAssertionCopy(), "readme.txt");
+
+		assertTrue("check can read Foo.java", fooJava.canRead());
+
+		assertFalse("check Bar.java doesn't exist", barJava.canRead());
+
+		assertTrue("check can read readme.txt", readmeTxt.canRead());
+
+		assertEquals("check readme.txt contents", "/readme.txt", FileUtils.fileRead(readmeTxt));
+	}
+
+	@Test
+	public void testCheckInCommandTest() throws Exception {
+		// Make sure that the correct files was checked out
+		File fooJava = new File(getWorkingCopy(), "src/main/java/Foo.java");
+
+		File barJava = new File(getWorkingCopy(), "src/main/java/Bar.java");
+
+		File readmeTxt = new File(getWorkingCopy(), "readme.txt");
+
+		assertFalse("check Foo.java doesn't yet exist", fooJava.canRead());
+
+		assertFalse("check Bar.java doesn't yet exist", barJava.canRead());
+
+		assertTrue("check can read readme.txt", readmeTxt.canRead());
+
+		// Change the files
+		createFooJava(fooJava);
+
+		createBarJava(barJava);
+
+		changeReadmeTxt(readmeTxt);
+
+		AddScmResult addResult = getScmManager().add(getScmRepository(),
+				new ScmFileSet(getWorkingCopy(), "src/main/java/Foo.java", null));
+
+		assertResultIsSuccess(addResult);
+
+		List<ScmFile> files = addResult.getAddedFiles();
+		assertNotNull(files);
+		assertEquals(1, files.size());
+		String fileWithoutPathUnixStyle = createFilePathWithoutCompletePath(getWorkingCopy().toString(),
+				files.get(0).getPath());
+		// SCM-998: filename separators not yet harmonized
+		assertEquals("/src/main/java/Foo.java", FilenameUtils.normalizeFilename(fileWithoutPathUnixStyle));
+
+		CheckInScmResult result = getScmManager().checkIn(getScmRepository(), new ScmFileSet(getWorkingCopy()),
+				"Commit message");
+
+		assertResultIsSuccess(result);
+
+		files = result.getCheckedInFiles();
+		assertNotNull(files);
+		assertEquals(2, files.size());
+
+		Map<String, ScmFile> fileMap = mapFilesByPath(files);
+		
+		ScmFile file1 = fileMap.get(StringUtils.replace(getWorkingCopy() + "/src/main/java/Foo.java", "\\", "/"));		
+		assertNotNull(file1);
+		assertEquals(ScmFileStatus.CHECKED_IN, file1.getStatus());
+
+		ScmFile file2 = fileMap.get(StringUtils.replace(getWorkingCopy() + "/readme.txt", "\\", "/"));
+		assertNotNull(file2);
+		assertEquals(ScmFileStatus.CHECKED_IN, file2.getStatus());
+
+		CheckOutScmResult checkoutResult = getScmManager().checkOut(getScmRepository(),
+				new ScmFileSet(getAssertionCopy()));
+
+		assertResultIsSuccess(checkoutResult);
+
+		fooJava = new File(getAssertionCopy(), "src/main/java/Foo.java");
+
+		barJava = new File(getAssertionCopy(), "src/main/java/Bar.java");
+
+		readmeTxt = new File(getAssertionCopy(), "readme.txt");
+
+		assertTrue("check can read Foo.java", fooJava.canRead());
+
+		assertFalse("check Bar.java doesn't exist", barJava.canRead());
+
+		assertTrue("check can read readme.txt", readmeTxt.canRead());
+
+		assertEquals("check readme.txt contents", "changed file", FileUtils.fileRead(readmeTxt));
+	}
+
+	private void createFooJava(File fooJava) throws Exception {
+		FileWriter output = new FileWriter(fooJava);
+
+		PrintWriter printer = new PrintWriter(output);
+		try {
+			printer.println("public class Foo");
+			printer.println("{");
+
+			printer.println("    public void foo()");
+			printer.println("    {");
+			printer.println("        int i = 10;");
+			printer.println("    }");
+
+			printer.println("}");
+		} finally {
+			IOUtil.close(output);
+			IOUtil.close(printer);
+		}
+	}
+
+	private void createBarJava(File barJava) throws Exception {
+		FileWriter output = new FileWriter(barJava);
+
+		PrintWriter printer = new PrintWriter(output);
+
+		printer.println("public class Bar");
+		printer.println("{");
+
+		printer.println("    public int bar()");
+		printer.println("    {");
+		printer.println("        return 20;");
+		printer.println("    }");
+
+		printer.println("}");
+
+		printer.close();
+
+		output.close();
+	}
+
+	private void changeReadmeTxt(File readmeTxt) throws Exception {
+		FileWriter output = null;
+
+		try {
+			output = new FileWriter(readmeTxt);
+
+			output.write("changed file");
+		} finally {
+			IOUtil.close(output);
+		}
+	}
+
+	private String createFilePathWithoutCompletePath(String workingcopyString, String relativePath) throws Exception {
+		String fileWithoutCompletePath = StringUtils.replace(relativePath, workingcopyString, "");
+		// for Windows compatibility
+		String fileWithoutPathUnixStyle = StringUtils.replace(fileWithoutCompletePath, "\\", "/");
+		return fileWithoutPathUnixStyle;
+	}
 }
--- src/test/java/org/apache/maven/scm/provider/svn/svnjava/command/diff/SvnJavaDiffCommandTckTest.java
+++ src/test/java/org/apache/maven/scm/provider/svn/svnjava/command/diff/SvnJavaDiffCommandTckTest.java
@@ -1,5 +1,9 @@
 package org.apache.maven.scm.provider.svn.svnjava.command.diff;
 
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+import static org.junit.Assert.assertTrue;
+
 /*
  * Licensed to the Apache Software Foundation (ASF) under one
  * or more contributor license agreements.  See the NOTICE file
@@ -20,9 +24,22 @@ package org.apache.maven.scm.provider.svn.svnjava.command.diff;
  */
 
 import java.io.File;
+import java.util.Iterator;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeSet;
 
+import org.apache.maven.scm.ScmFile;
+import org.apache.maven.scm.ScmFileSet;
+import org.apache.maven.scm.ScmTestCase;
+import org.apache.maven.scm.ScmVersion;
+import org.apache.maven.scm.command.diff.DiffScmResult;
+import org.apache.maven.scm.provider.ScmProvider;
 import org.apache.maven.scm.provider.svn.command.diff.SvnDiffCommandTckTest;
 import org.apache.maven.scm.provider.svn.svnjava.SvnJavaScmTestUtils;
+import org.apache.maven.scm.repository.ScmRepository;
+import org.codehaus.plexus.util.StringUtils;
+import org.junit.Test;
 
 /**
  * @author <a href="mailto:dh-maven@famhq.com">David Hawkins</a>
@@ -49,4 +66,116 @@ public class SvnJavaDiffCommandTckTest
     protected File getWorkingDirectory() {
         return super.getWorkingDirectory();
     }
+    
+    
+    @Test
+    public void testDiffCommand()
+        throws Exception
+    {
+        ScmRepository repository = getScmRepository();
+
+        // ----------------------------------------------------------------------
+        // Change the files
+        // ----------------------------------------------------------------------
+
+        //
+        // readme.txt is changed (changed file in the root directory)
+        // project.xml is added (added file in the root directory)
+        // src/test/resources is untouched (a empty directory is left untouched)
+        // src/test/java is untouched (a non empty directory is left untouched)
+        // src/test/java/org (a empty directory is added)
+        // src/main/java/org/Foo.java (a non empty directory is added)
+        //
+
+        // /readme.txt
+        ScmTestCase.makeFile( getWorkingCopy(), "/readme.txt", "changed readme.txt" );
+
+        // /project.xml
+        ScmTestCase.makeFile( getWorkingCopy(), "/project.xml", "changed project.xml" );
+
+        addToWorkingTree( getWorkingCopy(), new File( "project.xml" ), repository );
+
+        // /src/test/java/org
+        ScmTestCase.makeDirectory( getWorkingCopy(), "/src/test/java/org" );
+
+        addToWorkingTree( getWorkingCopy(), new File( "src/test/java/org" ), repository );
+
+        // /src/main/java/org/Foo.java
+        ScmTestCase.makeFile( getWorkingCopy(), "/src/main/java/org/Foo.java" );
+
+        addToWorkingTree( getWorkingCopy(), new File( "src/main/java/org" ), repository );
+
+        // src/main/java/org/Foo.java
+        addToWorkingTree( getWorkingCopy(), new File( "src/main/java/org/Foo.java" ), repository );
+
+        // ----------------------------------------------------------------------
+        // Diff the project
+        // ----------------------------------------------------------------------
+
+        ScmProvider provider = getScmManager().getProviderByUrl( getScmUrl() );
+        ScmFileSet fileSet = new ScmFileSet( getWorkingCopy() );
+        DiffScmResult result = provider.diff( repository, fileSet, null, (ScmVersion) null );
+
+        assertNotNull( "The command returned a null result.", result );
+
+        assertResultIsSuccess( result );
+
+        List<ScmFile> changedFiles = result.getChangedFiles();
+
+        Map<String, CharSequence> differences = result.getDifferences();
+
+        assertEquals( "Expected 3 files in the changed files list " + changedFiles, 3, changedFiles.size() );
+
+        assertEquals( "Expected 3 files in the differences list " + differences, 3, differences.size() );
+
+        // ----------------------------------------------------------------------
+        // Assert the files in the changed files list
+        // ----------------------------------------------------------------------
+
+        Iterator<ScmFile> files = new TreeSet<ScmFile>( changedFiles ).iterator();
+
+        //Check Foo.java
+        ScmFile file = files.next();
+
+        String fileWithoutPathUnixStyle = createFilePathWithoutCompletePath(getWorkingCopy().toString(),
+        		file.getPath());
+        
+        assertPath( "/src/main/java/org/Foo.java", fileWithoutPathUnixStyle );
+
+        assertTrue( file.getStatus().isDiff() );
+
+        String postRangeStr = "+/src/main/java/org/Foo.java\n\\ No newline at end of file\n";
+        String actualStr = differences.get( file.getPath() ).toString();
+        assertTrue( actualStr.endsWith( postRangeStr ) );
+
+        //Check readme.txt
+        file = files.next();
+
+        assertPath( "/readme.txt", file.getPath() );
+
+        assertTrue( file.getStatus().isDiff() );
+
+        postRangeStr =
+            "-/readme.txt\n\\ No newline at end of file\n+changed readme.txt\n\\ No newline at end of file\n";
+        actualStr = differences.get( file.getPath() ).toString();
+        assertTrue( actualStr.endsWith( postRangeStr ) );
+
+        //Check project.xml
+        file = files.next();
+
+        assertPath( "/project.xml", file.getPath() );
+
+        postRangeStr = "+changed project.xml\n\\ No newline at end of file\n";
+        actualStr = differences.get( file.getPath() ).toString();
+        assertTrue( actualStr.endsWith( postRangeStr ) );
+
+        assertTrue( file.getStatus().isDiff() );
+    }
+    
+	private String createFilePathWithoutCompletePath(String workingcopyString, String relativePath) throws Exception {
+		String fileWithoutCompletePath = StringUtils.replace(relativePath, workingcopyString, "");
+		// for Windows compatibility
+		String fileWithoutPathUnixStyle = StringUtils.replace(fileWithoutCompletePath, "\\", "/");
+		return fileWithoutPathUnixStyle;
+	}
 }
